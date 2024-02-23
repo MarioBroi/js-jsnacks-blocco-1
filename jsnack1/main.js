6986ec2c3e9b779fc1e02adbee4aff5140757700
MarioBroi <inoix16@gmail.com>
2024-02-23 11:20:30 +0000

    first commit

---
+++ jsnack1/main.js
@@ -0,0 +1,17 @@
+//console.log('Test');
+
+//- Chieder 2 numeri tramite prompt
+const numero1 = prompt('Inserisci un numero');
+const numero2 = prompt('Inserisci un secondo numero');
+//console.log(numero1, numero2);
+
+//- IF numero1 é MAGGIORE di numero2
+if (numero1 > numero2) {
+    //    - stampare in console numero1
+    console.log(numero1);
+
+    //- ELSE
+} else {
+    //    - stampare numero2
+    console.log(numero2);
+}
---
+++ jsnack1/readme.md
@@ -0,0 +1,16 @@
+# Instructions
+- L’utente inserisce due numeri in successione, con due prompt.
+- Il software stampa il maggiore.
+
+## Steps
+- Chieder 2 numeri tramite prompt
+- IF numero1 é MAGGIORE di numero2
+    - stampare in console numero1
+- ELSE
+    - stampare numero2
+
+### Tools
+- const / let
+- prompt
+- if / else
+- console.log
